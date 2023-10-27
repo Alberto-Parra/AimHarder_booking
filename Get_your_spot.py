a85f6e3f7eb1905fa29de817b33935d0b7c88930
alberto parra <alberto@parracastro.es>
2023-10-27 17:29:17 +0200

    coments and requirements added

--- Get_your_spot.py
+++ Get_your_spot.py
@@ -8,26 +8,37 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 from datetime import date, timedelta
 
+#load from .env file the user and password
 load_dotenv()
 user=os.getenv('USER')
 password=os.getenv('PASSWORD')
 
 def main():
+    
+    # open chrome windows
     driver = webdriver.Chrome()
+    
+    # login url
     url='https://aimharder.com/login'
+    
+    # go to this url
     driver.get(url)
 
+    # Write the username
     username = WebDriverWait(driver,3).until(EC.element_to_be_clickable((By.ID, 'mail')))
     username.clear()
     username.send_keys(user)
 
+    # Write the user password
     userpass = WebDriverWait(driver,1).until(EC.element_to_be_clickable((By.ID, 'pw')))
     userpass.clear()
     userpass.send_keys(password)
 
+    # Ones you are logged you can go directly to another part directly using the url easier than doing some clicking.
     reservas_url = 'https://project96.aimharder.com/schedule'
     driver.get(reservas_url)
 
+    # To book the spot in two day from now
     today=date.today()
     offset=timedelta(days=2)
     fecha_reserva=today+offset
@@ -35,13 +46,16 @@ def main():
     fecha_reserva_str=str(fecha_reserva.day)+'/'+str(fecha_reserva.month)+'/'+str(fecha_reserva.year)+'\n'
     fecha_reserva_str
 
+    # Write the date in the input
     select_day = WebDriverWait(driver,3).until(EC.element_to_be_clickable((By.ID, 'selw')))
     select_day.clear()
     select_day.send_keys(fecha_reserva_str)
 
+    # click in book class
     book_botton = WebDriverWait(driver,3).until(EC.element_to_be_clickable((By.XPATH, '(//a[text()="Reservar"])[1]')))
     book_botton.click()
     
+    #Close the browser
     time.sleep(3)
     driver.close()
 
